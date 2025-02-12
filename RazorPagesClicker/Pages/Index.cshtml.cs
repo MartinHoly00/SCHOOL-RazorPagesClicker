644ea873231000d06de0bf40b86f43bbcd647e68
Martin Holý <martin.holy.022@pslib.cz>
2025-02-12 12:30:49 +0100

    Add project files.

---
+++ RazorPagesClicker/Pages/Index.cshtml.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPagesClicker.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        [BindProperty]
+        public int Value { get; set; }
+
+        public void OnGet(int value)
+        {
+            Value = value;
+        }
+
+        public void OnGetChange(int value, int current)
+        {
+            Value = current + value;
+        }
+
+        public void OnGetReset(int value)
+        {
+            Value = value;
+        }
+    }
+}
